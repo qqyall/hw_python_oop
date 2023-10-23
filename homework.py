cf5c3c9da0ae33065187cd48df46e32689c20f91
Румянцев Савелий <radogask9rsd@gmail.com>
2023-10-23 15:57:39 +0500

    Fix TypeError bug with InfoMessage class

--- homework.py
+++ homework.py
@@ -8,19 +8,20 @@ class InfoMessage:
                  duration: float,
                  distance: float,
                  speed: float,
-                 calories: float):
+                 calories: float) -> None:
         self.training_type = training_type
-        self.duration = duration,
-        self.distance = distance,
-        self.speed = speed,
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
         self.calories = calories
 
     def get_message(self):
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        message: str = (f"Тип тренировки: {self.training_type}; "
+                        f"Длительность: {self.duration:.3f} ч.; "
+                        f"Дистанция: {self.distance:.3f} км; "
+                        f"Ср. скорость: {self.speed:.3f} км/ч; "
+                        f"Потрачено ккал: {self.calories:.3f}.")
+        return message
 
 
 class Training:
@@ -51,8 +52,9 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.action, self.duration, self.get_distance(),
-                           self.get_mean_speed(), self.get_spent_calories())
+        return InfoMessage(self.__class__.__name__, self.duration,
+                           self.get_distance(), self.get_mean_speed(),
+                           self.get_spent_calories())
 
 
 class Running(Training):
